cc7d8ba9caae2d22fd8370f781c190aeaf2a128f
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-26 15:25:34 +0530

    Create SecurityConfig.java

---
+++ src/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new JWTAuthenticationFilter(authenticationManager()));
+    }
+}
